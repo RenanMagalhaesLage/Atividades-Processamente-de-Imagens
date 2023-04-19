bac032463f9169279a79e49ca9e77b4f6922bac0
Renan Magalhães <renanlage50@gmail.com>
2023-04-18 21:32:54 -0300

    fourth commit

--- Filtros/filtros.c
+++ Filtros/filtros.c
@@ -98,15 +98,15 @@ void laplaciano(image In, image Out, int nl, int nc, int mn){
         for(int j = 1; j < nc - 1; j++)
         {
             sum = 0;
-            for(int y =-1; y <01; y++)
+            for(int y =-1; y <= 1; y++)
             {
                 for(int x = -1; x <= 1; x++)
                 {
-                    int px = (i+y) * nc + (j + x);
+                    int px = (i + y) * nc + (j + x);
                     sum += In[px] * laplaciano[y+1][x+1];
                 }
             }
-            if(abs(sum)>max){
+            if(abs(sum) > max){
                 max = abs(sum);
             }
             Out[i * nc + j] = abs(sum);
@@ -156,7 +156,8 @@ int main(int argc, char *argv[])
     printf("\n|                                                               |");
     printf("\n|       1) Imagem Negativa                                      |");
     printf("\n|       2) Convolucao / Embacar a imagem                        |");
-    printf("\n|       3) Laplaciano                                           |");
+    printf("\n|       3) Laplaciano - Fundo Preto                             |");
+    printf("\n|       4) Laplaciano - Fundo Branco                            |");
     printf("\n|       0) EXIT                                                 |");
     printf("\n|                                                               |");
     printf("\n+---------------------------------------------------------------+\n");
@@ -166,7 +167,7 @@ int main(int argc, char *argv[])
 
     /*Caso o usuario digitar algum número que esta fora das 
     opções exibe a seguinte mensagem:*/
-    while ( opcao < 0 || opcao > 3){
+    while ( opcao < 0 || opcao > 4){
         printf(" ATENCAO - Digite um numero que esteja entre as opcoes: \n");
         scanf("%d",&opcao);
     }
@@ -190,13 +191,21 @@ int main(int argc, char *argv[])
         break;
     case 3:
         laplaciano(In, Out, nr, nc, ml);
-        printf("oii\n");
         img_put(Out, nameOut, nr, nc, ml, GRAY);
         sprintf(cmd, "%s %s &", VIEW, nameOut);
         system(cmd);
         img_free(In);
         img_free(Out);
-        break;    
+        break;
+    case 4:
+        laplaciano(In, Out, nr, nc, ml);
+        negativo(Out, Out, nr, nc, ml);
+        img_put(Out, nameOut, nr, nc, ml, GRAY);
+        sprintf(cmd, "%s %s &", VIEW, nameOut);
+        system(cmd);
+        img_free(In);
+        img_free(Out);
+        break;      
     case 0:
         return 0;
     }
